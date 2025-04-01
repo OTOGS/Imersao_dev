68e6e9e21475e7b79670fa3d6e9e19f886752dc6
Otaniel G da Silva <94942202+OTOGS@users.noreply.github.com>
2025-04-01 19:20:53 +0000

    Inicio

---
+++ script.js
@@ -0,0 +1,12 @@
+idade = prompt("Qual a sua idade?")
+if(idade < 18){
+   alert('Você não pode jogar!')
+}
+if (idade >= 18){
+   escolhaJogador = prompt('Digite: 1 - Pedra, 2 - Papel, 3 - Tesoura:')
+   escolhaComputador = Math.floor(Math.random() * 3) + 1;
+   
+   if(escolhaJogador == escolhaComputador){
+      alert('Empate!!')
+   }
+}
