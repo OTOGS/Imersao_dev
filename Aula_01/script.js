3266a740a74127116fba9caf84875ef9b92a9286
Otaniel_G_da_Silva <otaniel.work2003@gmail.com>
2025-04-03 22:24:16 -0300

    Adição da aula 01

--- Aula_01/script.js
+++ Aula_01/script.js
@@ -1,13 +1,18 @@
-idade = prompt("Qual a sua idade?");
-if(idade < 18){
-   alert('Você não pode jogar!');
+function conversorWon(){
+    valor_em_won = parseFloat(prompt('Digite o valor em wons para converter em reais:'));
+    um_won_em_real = 0.0038634587;
+    valor_convertido = valor_em_won * um_won_em_real;
+    alert('₩ ' + valor_em_won.toLocaleString('pt-BR') + ' convertido em reais é R$ ' + valor_convertido.toLocaleString('pt-BR'));
 };
-if (idade >= 18){
-   escolhaJogador = prompt('Digite: 1 - Pedra, 2 - Papel, 3 - Tesoura:');
-   escolhaComputador = Math.floor(Math.random() * 3) + 1;
-   
-   if(escolhaJogador == escolhaComputador){
-      alert('Empate!!');
-   };
-}
-alert('Teste GIT');
+function conversorEuro(){
+    valor_em_euro = parseFloat(prompt('Digite o valor em euro para converter em reais:'));
+    um_euro_em_real = 6.3469;
+    valor_convertido = valor_em_euro * um_euro_em_real;
+    alert('€ ' + valor_em_euro.toLocaleString('pt-BR') + ' convertido em reais é R$ ' + valor_convertido.toLocaleString('pt-BR'));
+};
+function conversorDolar(){
+    valor_em_dolar = parseFloat(prompt('Digite o valor em dolar para converter em reais:'));
+    um_dolar_em_real = 5;
+    valor_convertido = valor_em_dolar * um_dolar_em_real;
+    alert('$ ' + valor_em_dolar.toLocaleString('pt-BR') + ' convertido em reais é R$ ' + valor_convertido.toLocaleString('pt-BR'));
+};
