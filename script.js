9c29f0e951a8fe2d1e07a3d2be2ed1b81fdf70f5
Otaniel_G_da_Silva <otaniel.work2003@gmail.com>
2025-04-03 21:46:08 -0300

    ponto_e_virgula

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-idade = prompt("Qual a sua idade?")
+idade = prompt("Qual a sua idade?");
 if(idade < 18){
-   alert('Você não pode jogar!')
-}
+   alert('Você não pode jogar!');
+};
 if (idade >= 18){
-   escolhaJogador = prompt('Digite: 1 - Pedra, 2 - Papel, 3 - Tesoura:')
+   escolhaJogador = prompt('Digite: 1 - Pedra, 2 - Papel, 3 - Tesoura:');
    escolhaComputador = Math.floor(Math.random() * 3) + 1;
    
    if(escolhaJogador == escolhaComputador){
-      alert('Empate!!')
-   }
+      alert('Empate!!');
+   };
 }
