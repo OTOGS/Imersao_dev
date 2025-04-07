91b6faf0bc288147f0dac7a6380da8c5a0a6a582
Otaniel <otaniel.work2003@gmail.com>
2025-04-07 19:18:37 -0300

    Início da Aula 03

--- Aula_03/script.js
+++ Aula_03/script.js
@@ -1,12 +1,17 @@
-idade = prompt("Qual a sua idade?");
-if(idade < 18){
-   alert('Você não pode jogar!');
+rodada = 1;
+while(rodada <= 3){
+  console.log('Rodada:' + rodada);
+  escolhaJogador = prompt('Nivel ' + rodada + ', vidro(1, 2 ou 3)?'
+  pisoQuebrado = Math.floor(Math.random() * 3 ) + 1;
+  if(escolhaJogador == pisoQuebrado){
+    alert('O vidro quebrou!!');
+    rodada = 999;
+  }else{
+    alert('Passou, piso quebrado estava na ' + pisoQuebrado)
+    rodada += 1;
+  };
 };
-if (idade >= 18){
-   escolhaJogador = prompt('Digite: 1 - Pedra, 2 - Papel, 3 - Tesoura:');
-   escolhaComputador = Math.floor(Math.random() * 3) + 1;
-   
-   if(escolhaJogador == escolhaComputador){
-      alert('Empate!!');
-   };
-};
+
+if(rodada == 4){
+  alert('Parabéns!! Você venceu!')
+}
