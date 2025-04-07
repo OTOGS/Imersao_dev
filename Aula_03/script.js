74b731020fc19be27de1bd6948ab46acb989af1e
Otaniel <otaniel.work2003@gmail.com>
2025-04-07 19:31:18 -0300

    Aula 03 concluída

--- Aula_03/script.js
+++ Aula_03/script.js
@@ -1,17 +1,19 @@
-rodada = 1;
-while(rodada <= 3){
-  console.log('Rodada:' + rodada);
-  escolhaJogador = prompt('Nivel ' + rodada + ', vidro(1, 2 ou 3)?'
-  pisoQuebrado = Math.floor(Math.random() * 3 ) + 1;
-  if(escolhaJogador == pisoQuebrado){
-    alert('O vidro quebrou!!');
-    rodada = 999;
-  }else{
-    alert('Passou, piso quebrado estava na ' + pisoQuebrado)
-    rodada += 1;
-  };
-};
+function jogar() {
+   rodada = 1;
+   while (rodada <= 3) {
+      console.log('Rodada:' + rodada);
+      escolhaJogador = prompt('Nivel ' + rodada + ', vidro(1, 2 ou 3)?');
+      pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+      if (escolhaJogador == pisoQuebrado) {
+         alert('O vidro quebrou!!');
+         rodada = 999;
+      } else {
+         alert('Passou, piso quebrado estava na ' + pisoQuebrado)
+         rodada += 1;
+      };
+   };
 
-if(rodada == 4){
-  alert('Parabéns!! Você venceu!')
+   if (rodada == 4) {
+      alert('Parabéns!! Você venceu!')
+   }
 }
